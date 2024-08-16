0e7f1a055da7fd8337381dee001eab58b712e991
SUNIL KUMAR PAL <1996sunilkumarpal@gmail.com>
2024-08-16 13:40:47 +0530

    Update HealthCare_Analytics.sql

--- HealthCare_Analytics_MySQL/HealthCare_Analytics.sql
+++ HealthCare_Analytics_MySQL/HealthCare_Analytics.sql
@@ -3,12 +3,15 @@
 --where table_name = 'Healcare_Analytics'
 
 
---Cleaning the Appointments Table Data
+Cleaning the Appointments Table Data
+```sql
+    
 select *, datepart(year,appointmentdate)Appointment_Year,
 datename(month, appointmentdate)Appointment_Month,
 datepart(day, appointmentdate)Appointment_Day,
 CONVERT(date, appointmentdate)Appointment_time
 from Appointments
+    ```
 
 --Cleaning the Billing Table Data
 select *,datepart(year, billingdate)Billing_year,
